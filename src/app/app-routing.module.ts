9226ea4e4108d498dc0df34a7dc84afe43a3cc90
Navdeep-softuvo <86232316+Navdeep-softuvo@users.noreply.github.com>
2021-08-19 12:58:24 +0530

    create profile page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -65,6 +65,10 @@ const routes: Routes = [
   {
     path: 'profile',
     loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'edit-profile',
+    loadChildren: () => import('./edit-profile/edit-profile.module').then( m => m.EditProfilePageModule)
   }
 ];
 @NgModule({
